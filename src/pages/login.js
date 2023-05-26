c1880728cc78aa75208ab8e41afc19e94c640d83
BenjaminHerrera <BenHerrera1044@outlook.com>
2023-05-26 14:36:36 -0700

    Appearance Updates

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,15 +1,11 @@
 // React and Next.js imports
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import { useRouter } from 'next/router';
 import React from "react";
 
 // Toaster related imports
 import { errorToaster } from '@/components/toasters';
 import { ToastContainer } from 'react-toastify';
-
-// Particles imports
-import { loadSlim } from "tsparticles-slim";
-import Particles from "react-tsparticles";
 
 // JS Cookies import
 import Cookies from 'js-cookie';
@@ -18,68 +14,6 @@ import Cookies from 'js-cookie';
 const Login = () => {
   // Variable declaration and initialization
   const router = useRouter();
-
-  // Particle settings 
-  const particlesSettings = {
-    background: {
-      color: {
-        value: "#0d47a1",
-      },
-      opacity: 0
-    },
-    fpsLimit: 120,
-    interactivity: {
-      events: {
-        resize: true,
-      },
-      modes: {
-        push: {
-          quantity: 4,
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4,
-        },
-      },
-    },
-    particles: {
-      color: {
-        value: "#808080",
-      },
-      move: {
-        directions: "none",
-        enable: true,
-        outModes: {
-          default: "bounce",
-        },
-        random: false,
-        speed: 1,
-        straight: false,
-      },
-      number: {
-        density: {
-          enable: true,
-          area: 800,
-        },
-        value: 300,
-      },
-      opacity: {
-        value: 0.5,
-      },
-      shape: {
-        type: "circle",
-      },
-      size: {
-        value: { min: 1, max: 5 },
-      },
-    },
-    detectRetina: true,
-  }
-
-  // Particle engine initialization
-  const particlesInit = useCallback(async engine => {
-    await loadSlim(engine);
-  }, []);
 
   // Pop toaster message if noTokens cookie is present
   useEffect(() => {
@@ -160,20 +94,15 @@ const Login = () => {
 
   // Component return
   return (
-    <div className="relative flex items-center justify-center bg-gradient-to-tr from-blue1 to-white h-screen">
-      {/* <Particles
-        id="tsparticles"
-        init={particlesInit}
-        options={particlesSettings}
-      /> */}
-      <div className="bg-white rounded-xl shadow-2xl h-1/4 w-1/7">
-        <div className="px-5 h-full flex flex-col justify-around">
+    <div className="relative flex items-center justify-center bg-gradient-to-tr from-blue1 to-sky h-screen">
+      <div className="bg-white/[0.6] rounded-2xl shadow-lg shadow-sky h-fit w-fit py-5">
+        <div className="px-5 h-full flex flex-col gap-5">
           <div className="text-3xl">
             WingSuite Login
           </div>
           <div className="">
             <input 
-              className="shadow-lg border border-silver rounded text-gray-700 w-full focus:outline-none focus:shadow-outline p-1" 
+              className="bg-white/[0.3] border border-silver rounded-lg shadow-lg text-gray-700 w-full focus:outline-none focus:shadow-outline p-1" 
               id="email" 
               type="text" 
               placeholder="Email" 
@@ -181,7 +110,7 @@ const Login = () => {
           </div>
           <div className="">
             <input 
-              className="shadow-lg border border-silver rounded text-gray-700 w-full focus:outline-none focus:shadow-outline p-1" 
+              className="bg-white/[0.3] border border-silver rounded-lg shadow-lg text-gray-700 w-full focus:outline-none focus:shadow-outline p-1" 
               id="password" 
               type="password"
               placeholder="Password" 
@@ -189,8 +118,8 @@ const Login = () => {
           </div>
           <button 
             onClick={loginPress} 
-            className="w-1/4 text-lg bg-sky rounded-md drop-shadow-lg hover:bg-darksky hover:-translate-y-[0.1rem] hover:shadow-lg">
-            Login
+            className="text-lg bg-sky/[0.8] rounded-md drop-shadow-lg px-3 py-0.5 w-fit hover:bg-white/[0.3] hover:-translate-y-[0.1rem] hover:shadow-xl">
+            Log In
           </button>
         </div>
       </div>

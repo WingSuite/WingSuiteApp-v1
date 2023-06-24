2d84ad01a3fe6eeadeec4a4d96095d6e9f88b26e
BenjaminHerrera <BenHerrera1044@outlook.com>
2023-06-24 16:29:11 -0700

    Added refresh feature (hesitant)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,7 @@ import Cookies from "js-cookie";
 import { useEffect } from "react";
 
 // Util imports
-import { get } from "@/utils/call";
+import { get, post } from "@/utils/call";
 
 // Base page definition
 export default function App({ Component, pageProps }) {
@@ -17,10 +17,21 @@ export default function App({ Component, pageProps }) {
   (async () => {
     // If the access token is not there, don't get the user's whoami
     const access = Cookies.get("access");
+    const refresh = Cookies.get("refresh");
     if (access != undefined) {
-        // Using the given access token, store the information
+      // Using the given access token, store the information
       // from the response of the /user/who_am_i/ endpoint
-      const res = await get("/user/who_am_i/", access);
+      var res = await get("/user/who_am_i/", access);
+
+      // If the user's access token is expired, refresh
+      if (res.status == "expired") {
+        // Get new access token
+        const new_access = await post("/auth/refresh/", {}, refresh);
+        Cookies.set("access", new_access.access_token);
+
+        // Refresh who_am_i value
+        var res = await get("/user/who_am_i/", access);
+      }
 
       // Store the content of the result to local storage
       localStorage.setItem("whoami", JSON.stringify(res));

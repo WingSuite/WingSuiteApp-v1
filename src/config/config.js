d4e60ab837e0db00070c097173a9498620831a14
BenjaminHerrera <BenHerrera1044@outlook.com>
2023-10-15 20:48:40 -0700

    Added blank slate for tasks page

--- src/config/config.js
+++ src/config/config.js
@@ -9,6 +9,7 @@ import {
   VscListFlat,
   VscKey,
   VscOrganization,
+  VscChecklist
 } from "react-icons/vsc";
 import { BsPerson } from "react-icons/bs";
 import { BiNews } from "react-icons/bi";
@@ -217,6 +218,11 @@ export const regularSidebarContents = [
     title: "Home Page",
     link: "/dashboard/homepage",
     icon: <VscHome />,
+  },
+  {
+    title: "Tasks",
+    link: "/dashboard/tasks",
+    icon: <VscChecklist />,
   },
   {
     title: "Notifications",
